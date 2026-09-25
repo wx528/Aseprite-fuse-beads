9fa912e0c8e216ea46020e2a827354c96e15efa9
wx528 <33819629+wx528@users.noreply.github.com>
2026-09-25 23:02:30 +0800

    Revert "Bigger bolder bead codes: wider fit caps, bold strokes at scale>=2"
    This reverts commit 6fb4b31b4bf21be3e56a8b159ce84f21ba4d01ea.

--- src/font.lua
+++ src/font.lua
@@ -47,7 +47,7 @@ function Font.measure(text, scale)
   return (#text * 4 - 1) * scale, 5 * scale
 end
 
-function Font.draw(img, x, y, text, scale, color, bold)
+function Font.draw(img, x, y, text, scale, color)
   local cx = x
   for i = 1, #text do
     local g = Font.GLYPHS[text:sub(i, i)]
@@ -59,9 +59,6 @@ function Font.draw(img, x, y, text, scale, color, bold)
             for sy = 0, scale - 1 do
               for sx = 0, scale - 1 do
                 img:drawPixel(cx + (col - 1) * scale + sx, y + (row - 1) * scale + sy, color)
-                if bold then
-                  img:drawPixel(cx + (col - 1) * scale + sx + 1, y + (row - 1) * scale + sy, color)
-                end
               end
             end
           end
